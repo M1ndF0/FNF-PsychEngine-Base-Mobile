5d8928b7a9eca0e2487c0470813f13b9542c04b8
M1ndF0 <lerceybteam09@gmail.com>
2024-09-14 14:27:47 +0700

    Add files via upload

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -298,7 +298,7 @@ class TitleState extends MusicBeatState
 		ngSpr.screenCenter(X);
 		ngSpr.antialiasing = ClientPrefs.data.antialiasing;
 		
-		teamSpr = new FlxSprite(0, FlxG.height * 0.35).loadGraphic(Paths.image('team'));
+		teamSpr = new FlxSprite(0, FlxG.height * 0.425).loadGraphic(Paths.image('team'));
 		add(teamSpr);
 		teamSpr.visible = false;
 		teamSpr.setGraphicSize(Std.int(teamSpr.width * 0.8));
